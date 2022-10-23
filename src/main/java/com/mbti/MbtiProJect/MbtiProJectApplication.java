98822b00c2bc4ac78fec2b043f2cb488eff4341f
주동호 <100278301+JOODON@users.noreply.github.com>
2022-10-23 19:29:23 +0900

    MBTI프로젝트 다시시작!

---
+++ src/main/java/com/mbti/MbtiProJect/MbtiProJectApplication.java
@@ -0,0 +1,13 @@
+package com.mbti.MbtiProJect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiProJectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtiProJectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbti/MbtiProJect/MbtiProJectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbti.MbtiProJect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiProJectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
